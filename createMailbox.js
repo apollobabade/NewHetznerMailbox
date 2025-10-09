63ed929462c1d13e3b61554fa751448cbd5aed17
Apollo Babade <apollobabade@gmail.com>
2025-10-09 18:12:40 +0300

    Handle Errors

--- createMailbox.js
+++ createMailbox.js
@@ -233,6 +233,7 @@ async function createMailbox(freelancer) {
     page.click('input[type="submit"][value="Save"]'),
   ]);
 
+  /* ---------------------- Handle Errors ---------------------- */
   /* ---------------------- Confirmation ---------------------- */
   const resultMessage = await retry(page, async () => {
     const msg = await page.evaluate(() => {
