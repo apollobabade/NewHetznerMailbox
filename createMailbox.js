aadf030eb210501fb91385b303c65feffa8bc2b5
Apollo Babade <apollobabade@gmail.com>
2025-10-09 17:56:49 +0300

    Changed 's'

--- createMailbox.js
+++ createMailbox.js
@@ -233,7 +233,7 @@ async function createMailbox(freelancer) {
     page.click('input[type="submit"][value="Save"]'),
   ]);
 
-  /* ---------------------- Confirmation ---------------------- */
+  /* ---------------------- Confirmations ---------------------- */
   const resultMessage = await retry(page, async () => {
     const msg = await page.evaluate(() => {
       const okBox = document.querySelector('div.ok');
